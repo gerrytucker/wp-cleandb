2307c9c92936fe699c00ff908af8bf38a93e8202
Gerry Tucker <gerrytucker@gerrytucker.co.uk>
2015-04-12 22:18:46 +0100

    Initial commit.

---
+++ wp-cleandb.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Plugin Name: WP-CleanDB
+Plugin URI: https://github.com/gerrytucker/wp-cleandb
+Description: Cleanup your Wordpress database in one click!
+Version: 1.0.0
+Author: Gerry Tucker
+Author URI: http://gerrytucker.co.uk
+GitHub Plugin URI: gerrytucker/wp-cleandb
+GitHub Branch: master
+*/
+
+function cleandb_admin() {
+	include('wp-cleandb-admin.php');
+}
+
+function cleandb_admin_actions() {
+	add_options_page('WP-CleanDB', 'WP-CleanDB', 'administrator', 'WP-CleanDB', 'cleandb_admin');
+}
+
+add_action('admin_menu', 'cleandb_admin_actions');
+?>
